c02937ada15584e7856303336279dcb08b74f361
Carlos Esquivel <carlosrem70@gmail.com>
2025-05-16 02:12:52 -0600

    Living room decorated
    Plus reordering a ton of assets

--- Assets/_Game/Scripts/InteractableObject.cs
+++ Assets/_Game/Scripts/InteractableObject.cs
@@ -1,28 +1,43 @@
 using System;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.Rendering.Universal;
 
 [RequireComponent(typeof(Collider2D))]
 public class InteractableObject : MonoBehaviour
 {
+    [SerializeField] private bool disableOnStart;
     [SerializeField] private UnityEvent onEnterRange;
     [SerializeField] private UnityEvent onExitRange;
     [SerializeField] private UnityEvent onInteracted;
     [SerializeField] private float delayAfterInteracted = 0.5f;
     [SerializeField] private bool disableAfterInteracted;
     private float _delayTimer;
-    
+
+    [SerializeField] private Light2D lightMarker;
     private Collider2D _collider;
 
     public void Awake()
     {
         _collider = GetComponent<Collider2D>();
+
+        if (disableOnStart)
+        {
+            enabled = false;
+        }
+    }
+
+    private void OnEnable()
+    {
+        _collider.enabled = true;
+        lightMarker.SetActive(true);
     }
 
     private void OnDisable()
     {
         onExitRange?.Invoke();
         _collider.enabled = false;
+        lightMarker.SetActive(false);
     }
 
     private void Update()
